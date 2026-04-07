491fd136ac23ae7597c20837299df6394f50993e
_HDH <45970570+hdh06@users.noreply.github.com>
2026-04-06 21:18:31 -0700

    Add testing for PostStore.java

---
+++ TP2/HW2SourceCode/src/entityClasses/PostStoreTest.java
@@ -0,0 +1,197 @@
+package entityClasses;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.junit.jupiter.api.*;
+
+public class PostStoreTest {
+	
+	static PostStore testPostStore;
+
+	public PostStoreTest() {
+		
+	}
+	
+	@BeforeEach
+	public void setUp() {
+		testPostStore = new PostStore();
+		
+		for (int i = 0; i < 10; i++)
+			testPostStore.addPost(new Post(i, "" , "", "" , ""));
+	}
+	
+	@AfterEach 
+	public void tearDown() {
+		testPostStore.hardReset();
+	}
+	
+	@Test
+	public void BoundaryTest1() {
+		Post returnObj = testPostStore.getPostList().get(0);
+
+		int expectedID = 0;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+
+	@Test
+	public void BoundaryTest2() {
+		Post returnObj = testPostStore.getPostList().get(1);
+
+		int expectedID = 1;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+
+	@Test
+	public void BoundaryTest3() {
+		Post returnObj = testPostStore.getPostList().get(5);
+
+		int expectedID = 5;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+
+	@Test
+	public void BoundaryTest4() {
+		Post returnObj = testPostStore.getPostList().get(8);
+
+		int expectedID = 8;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+
+	@Test
+	public void BoundaryTest5() {
+		Post returnObj = testPostStore.getPostList().get(9);
+
+		int expectedID = 9;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+
+	@Test
+	public void BoundaryTest6() {
+		try {
+			Post returnObj = testPostStore.getPostList().get(-1);
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Index -1 out of bounds for length 10");
+		}
+	}
+		
+	@Test
+	public void BoundaryTest7() {
+		try {
+			Post returnObj = testPostStore.getPostList().get(10);
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Index 10 out of bounds for length 10");
+		}
+	}
+	
+	@Test
+	public void DeleteAPost1() {
+		testPostStore.deletePost(testPostStore.getPostList().get(0));
+		
+		Post returnObj = testPostStore.getPostList().get(0);
+		
+		int expectedID = 1;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+	
+	@Test
+	public void DeleteAPost2() {
+		testPostStore.deletePost(testPostStore.getPostList().get(0));
+		
+		Post returnObj = testPostStore.getPostList().get(1);
+		
+		int expectedID = 2;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+	
+	@Test
+	public void DeleteAPost3() {
+		testPostStore.deletePost(testPostStore.getPostList().get(0));
+		
+		Post returnObj = testPostStore.getPostList().get(5);
+		
+		int expectedID = 6;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+	
+	@Test
+	public void DeleteAPost4(){
+		testPostStore.deletePost(testPostStore.getPostList().get(0));
+		
+		Post returnObj = testPostStore.getPostList().get(8);
+		
+		int expectedID = 9;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+	
+	@Test
+	public void DeleteAPost5() {
+		try {
+			testPostStore.deletePost(testPostStore.getPostList().get(0));
+		
+			Post returnObj = testPostStore.getPostList().get(9);
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Index 9 out of bounds for length 9");
+		}
+	}
+	
+	@Test 
+	public void AddAPost1() {
+		testPostStore.addPost(new Post(10, "", "", "", ""));
+		
+		Post returnObj = testPostStore.getPostList().get(10);
+		
+		int expectedID = 10;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+	
+	@Test 
+	public void AddAPost2() {
+		testPostStore.addPost(new Post(10, "", "", "", ""));
+		
+		Post returnObj = testPostStore.getPostList().get(9);
+		
+		int expectedID = 9;
+		
+		assertEquals(returnObj.getId(), expectedID);
+	}
+	
+	@Test 
+	public void AddAPost3() {
+		testPostStore.addPost(new Post(10, "", "", "", ""));
+		
+		try {
+			Post returnObj = testPostStore.getPostList().get(11);
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Index 11 out of bounds for length 11");
+		}
+	}
+	
+	@Test
+	public void UnreadPost1() {
+		ArrayList<Post> UnreadList = testPostStore.getUnreadPosts("dummy");
+		
+		assertEquals(testPostStore.getPostList().size(), UnreadList.size());
+	}
+	
+	@Test 
+	public void UnreadPost2() {
+		testPostStore.retrieve(0).markAsRead("dummy");
+		testPostStore.retrieve(9).markAsRead("dummy");
+		ArrayList<Post> UnreadList = testPostStore.getUnreadPosts("dummy");
+		
+		assertEquals(testPostStore.getPostList().size() - 2, UnreadList.size());
+	}
+}
