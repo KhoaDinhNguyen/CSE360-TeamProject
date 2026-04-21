97a5d4b15145545026bec291648dc4723dc8be82
_HDH <45970570+hdh06@users.noreply.github.com>
2026-04-20 20:25:15 -0700

    Fix: add some comments

--- TP3/TP3SourceCode/src/guiGradingSystem/Assignment.java
+++ TP3/TP3SourceCode/src/guiGradingSystem/Assignment.java
@@ -111,6 +111,7 @@ public class Assignment {
 	/**
 	 * Constructor to create an assignment with tile, maxScore, score and weight
 	 * @param title A string contain the title of the assignment
+	 * @param content A string contain the content of the assignment
 	 * @param maxScore the maximum integer score of the assignment 
 	 * @param weight the percentage weight of the assignment
 	 */
